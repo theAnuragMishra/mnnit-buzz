e41ed1e09ad78769188776b87480b1e777c5a9e2
shreya <shreyapunganur05@gmail.com>
2024-04-11 00:29:48 +0530

    diary updated

--- components/shreya/diary.tsx
+++ components/shreya/diary.tsx
@@ -1,11 +1,74 @@
-export default function Messages(){
-    return(
-        <div>
-
-diary updated again 
-
-
-
+export default function Messages() {
+    return (
+      <div>
+        diary updated again
+        <div className="bg-blue w-[full]">
+          <div className="flex justify-center">
+            <div className="w-[90%]">
+              <div className="flex justify-between items-center py-5">
+                <div className="text-2xl font-bold">Diary</div>
+                <div className="flex">
+                  <div className="mr-5">
+                    <button className="bg-white text-blue-500 px-3 py-1 rounded-lg">
+                      New Entry
+                    </button>
+                  </div>
+                  <div>
+                    <button className="bg-white text-blue-500 px-3 py-1 rounded-lg">
+                      View All
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <div className="flex justify-between items-center py-5">
+                <div className="text-2xl font-bold">Diary</div>
+                <div className="flex">
+                  <div className="mr-5">
+                    <button className="bg-white text-blue-500 px-3 py-1 rounded-lg">
+                      New Entry
+                    </button>
+                  </div>
+                  <div>
+                    <button className="bg-white text-blue-500 px-3 py-1 rounded-lg">
+                      View All
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <div className="flex justify-between items-center py-5">
+                <div className="text-2xl font-bold">Diary</div>
+                <div className="flex">
+                  <div className="mr-5">
+                    <button className="bg-white text-blue-500 px-3 py-1 rounded-lg">
+                      New Entry
+                    </button>
+                  </div>
+                  <div>
+                    <button className="bg-white text-blue-500 px-3 py-1 rounded-lg">
+                      View All
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <div className="flex justify-between items-center py-5">
+                <div className="text-2xl font-bold">Diary</div>
+                <div className="flex">
+                  <div className="mr-5">
+                    <button className="bg-white text-blue-500 px-3 py-1 rounded-lg">
+                      New Entry
+                    </button>
+                  </div>
+                  <div>
+                    <button className="bg-white text-blue-500 px-3 py-1 rounded-lg">
+                      View All
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <div className="flex justify-between items-center py"></div>
+            </div>
+          </div>
         </div>
-    )
-}
+      </div>
+    );
+  }
